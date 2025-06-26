335d327f5f85bb1592d01620cfd71b0759407532
rhoneythseabra <rhoneyholanda@gmail.com>
2025-06-25 21:32:53 -0300

    Exercício média

---
+++ atividade02/media-escolar.py
@@ -0,0 +1,7 @@
+nota1 = float(input("nota 1: "))
+nota2 = float(input("nota 2: "))
+nota3 = float(input("nota 3: "))
+
+media_notas = (nota1 + nota2 + nota3) / 3
+
+print(f"Nota 1: {nota1} + Nota 2: {nota2} + Nota 3: {nota3} dá uma Média de: {media_notas:.2f}")
